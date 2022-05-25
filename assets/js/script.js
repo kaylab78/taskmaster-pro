bbeb769d3ba8cd7835d8acb320e6d18a3c20cba1
kaylab78 <kaylabackus78@gmail.com>
2022-05-25 17:21:28 -0500

    Added drag and drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -225,3 +225,18 @@ $(".card .list-group").sortable({
     console.log(tempArr);
   }
 });
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
